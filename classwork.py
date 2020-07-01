5d309546c300d6cd1046a87722ffc34fe9a5ec66
elmo33 <saba.momtselidze.1@btu.edu.ge>
2020-07-01 22:39:47 +0400

    nn

--- classwork.py
+++ classwork.py
@@ -3,20 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'group 1.2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books_db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
-class Books(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30), nullable=False)
-    author = db.Column(db.String(30), nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    username = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
 
     def __str__(self):
-        return f'Book title:{self.title}; Author: {self.author}; Price: {self.price}'
+        return f'Username: {self.username}, Password: {self.password}'
 
 
 # db.create_all()
@@ -38,42 +37,44 @@ def home():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        user = request.form['user']
-        session['username'] = user
+        email = request.form['email']
+        session['email'] = email
         return redirect(url_for('user'))
+
     else:
-        if 'user' in request.args:
-            user = request.args['user']
-            return f'{user}'
+        if 'email' in request.args and 'password' in request.args:
+            print(request.args["password"])
+            email = request.args['email']
+            return f'{email}'
         return render_template('login.html')
 
 
-@app.route('/user')
-def user():
-    if 'username' in session:
-        return f"Hello {session['username']}"
-    else:
-        return redirect(url_for('login'))
+# @app.route('/user')
+# def user():
+#     if 'username' in session:
+#         return f"Hello {session['username']}"
+#     else:
+#         return redirect(url_for('login'))
+
+#
+# @app.route('/logout')
+# def logout():
+#     session.pop('username', None)
+#     return 'you are logged out'
 
 
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return 'you are logged out'
-
-
-@app.route('/books', methods=['GET', 'POST'])
-def books():
-    if request.method == 'POST':
-        t = request.form['title']
-        a = request.form['author']
-        p = float(request.form['price'])
-        b1 = Books(title=t, author=a, price=p)
-        db.session.add(b1)
-        db.session.commit()
-        return 'წიგნი დამატებულია'
-
-    return render_template('books.html')
+# @app.route('/books', methods=['GET', 'POST'])
+# def books():
+#     if request.method == 'POST':
+#         t = request.form['title']
+#         a = request.form['author']
+#         p = float(request.form['price'])
+#         b1 = Books(title=t, author=a, price=p)
+#         db.session.add(b1)
+#         db.session.commit()
+#         return 'წიგნი დამატებულია'
+#
+#     return render_template('books.html')
 
 
 if __name__ == '__main__':
