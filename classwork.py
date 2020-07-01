597c36031dc8ac3ab60897aabb31152c4d202337
Saba <saabelmosaba@gmail.com>
2020-07-02 01:07:48 +0400

    FINAL

--- classwork.py
+++ classwork.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import time
+
+# მარტივი ვებსაიტი, შედგება 5 გვერდისგან
+# პირველ გვერდზე განთავსებულია სურათი, რომელიც ხსნის ვებსაიტის შინაარს
+# ლოგინ გვერდის მთავარი დანიშნულებაა შეყვანილი იმეილის და პაროლის წამოღება და მონაცემთა ბაზაში შენახვა
+# დამატების დაკლიკების შემდეგ გადმოვყავვართ მთავარ გვერდზე შეტყობინებით რომ მონაცემები წარმატებით დაემატა
+# Cool stuff გვერდზე განთავსებულია ღილაკი რომელზე დაჭერის შემდეგ ჩაიტვირთება loading icon
+# Users- გვერდზე გამოქვეყნებულია ყველა ის მონაცემი, რომელიც შეყვანილი მომხარებლის მონაცემთა ბაზაში
+# ბოლოს კი Html-ში ჯავასკრიპტით აწყობილი კავკულატორი, რომელიც რა თქმა უნდა მე არ ამიწყვია
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'group 1.2'
+app.config['SECRET_KEY'] = 'vitomkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -11,22 +20,14 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
     password = db.Column(db.String(30), nullable=False)
 
     def __str__(self):
-        return f'Username: {self.username}, Password: {self.password}'
+        return f'Email: {self.email}, Password: {self.password}'
 
 
-# db.create_all()
-# b1 = Books.query.first()
-# print(b1)
-# all_books = Books.query.all()
-# for each in all_books:
-#     print(each)
-# b1 = Books(title='სიბრძნე სიცრუისა', author='სულხან-საბა ორბელიანი', price=15)
-# db.session.add(b1)
-# db.session.commit()
+db.create_all()
 
 
 @app.route('/')
@@ -38,43 +39,33 @@ def home():
 def login():
     if request.method == 'POST':
         email = request.form['email']
-        session['email'] = email
-        return redirect(url_for('user'))
+        password = request.form['password']
+        print(email)
+        print(password)
+        b1 = User(email=email, password=password)
+        db.session.add(b1)
+        db.session.commit()
+        return render_template("index.html", text="Successfully added your info to our list")
 
     else:
-        if 'email' in request.args and 'password' in request.args:
-            print(request.args["password"])
-            email = request.args['email']
-            return f'{email}'
         return render_template('login.html')
 
 
-# @app.route('/user')
-# def user():
-#     if 'username' in session:
-#         return f"Hello {session['username']}"
-#     else:
-#         return redirect(url_for('login'))
-
-#
-# @app.route('/logout')
-# def logout():
-#     session.pop('username', None)
-#     return 'you are logged out'
+@app.route("/cool_stuff", methods=["GET", "POST"])
+def cool_stuff():
+    if request.method == "POST":
+        return render_template("cool_stuff.html", waiting=True, function=time)
+    return render_template("cool_stuff.html")
 
 
-# @app.route('/books', methods=['GET', 'POST'])
-# def books():
-#     if request.method == 'POST':
-#         t = request.form['title']
-#         a = request.form['author']
-#         p = float(request.form['price'])
-#         b1 = Books(title=t, author=a, price=p)
-#         db.session.add(b1)
-#         db.session.commit()
-#         return 'წიგნი დამატებულია'
-#
-#     return render_template('books.html')
+@app.route("/calculator")
+def calculator():
+    return render_template("calculator.html")
+
+
+@app.route("/users", methods=["GET"])
+def users():
+    return render_template("users.html", user_list=User.query.all())
 
 
 if __name__ == '__main__':
